aaddb6c3c24b98bd828728a163c8da497af31ae1
Sergio Diuve <diuve@example.com>
2025-07-21 19:29:58 -0600

    backend

---
+++ backend/users/urls.py
@@ -0,0 +1,8 @@
+# users/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Aquí irán las URLs específicas de la app users
+    # Por ejemplo: path('register/', views.register, name='register'),
+]
